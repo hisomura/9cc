22f9585286ec53d77448b170c7486ac6368f03bb
hisomura <hisomura.noreply@gmail.com>
2020-04-21 08:07:05 +0900

    コンパイラが壊れてることに気が付いたのでconsume()の実装までを全部巻き戻し

--- 9cc.c
+++ 9cc.c
@@ -22,7 +22,6 @@ struct Token
   Token *next;    // 次の入力トークン
   int val;        // kindがTK_NUMの場合、その数値
   char *str;      // トークン文字列
-  int len;        // トークンの長さ
 };
 
 // 現在着目しているトークン
@@ -39,12 +38,6 @@ typedef enum
   ND_MUL, // *
   ND_DIV, // /
   ND_NUM, // 整数
-  ND_EQ,  // ==
-  ND_NE,  // !=
-  ND_GT,  // >
-  ND_GE,  // >=
-  ND_LE,  // <=
-  ND_LT   // <
 } NodeKind;
 
 typedef struct Node Node;
@@ -59,13 +52,10 @@ struct Node
 };
 
 // プロトタイプ宣言
-Node *expr();
-Node *equality();
-Node *relational();
-Node *add();
-Node *mul();
-Node *unary();
 Node *primary();
+Node *mul();
+Node *expr();
+Node *unary();
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
 {
@@ -101,11 +91,9 @@ void error_at(char *loc, char *fmt, ...)
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
-bool consume(char *op)
+bool consume(char op)
 {
-  if (token->kind != TK_RESERVED ||
-      strlen(op) != token->len ||
-      memcmp(token->str, op, token->len))
+  if (token->kind != TK_RESERVED || token->str[0] != op)
     return false;
   token = token->next;
   return true;
@@ -113,11 +101,9 @@ bool consume(char *op)
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進める。
 // それ以外の場合にはエラーを報告する。
-void expect(char *op)
+void expect(char op)
 {
-  if (token->kind != TK_RESERVED ||
-      strlen(op) != token->len ||
-      memcmp(token->str, op, token->len))
+  if (token->kind != TK_RESERVED || token->str[0] != op)
     error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
@@ -133,24 +119,18 @@ int expect_number()
   return val;
 }
 
-Node *expr()
+Node *primary()
 {
-  return add();
-}
-
-Node *add()
-{
-  Node *node = mul();
-
-  for (;;)
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume('('))
   {
-    if (consume('+'))
-      node = new_node(ND_ADD, node, mul());
-    else if (consume('-'))
-      node = new_node(ND_SUB, node, mul());
-    else
-      return node;
+    Node *node = expr();
+    expect(')');
+    return node;
   }
+
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
 }
 
 Node *mul()
@@ -168,27 +148,27 @@ Node *mul()
   }
 }
 
-Node *unary()
+Node *expr()
 {
+  Node *node = mul();
+
+  for (;;)
+  {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *unary() {
   if (consume('+'))
     return primary();
   if (consume('-'))
     return new_node(ND_SUB, new_node_num(0), primary());
   return primary();
-}
-
-Node *primary()
-{
-  // 次のトークンが"("なら、"(" expr ")"のはず
-  if (consume('('))
-  {
-    Node *node = expr();
-    expect(')');
-    return node;
-  }
-
-  // そうでなければ数値のはず
-  return new_node_num(expect_number());
 }
 
 bool at_eof()
