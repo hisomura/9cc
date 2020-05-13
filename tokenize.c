e8d44fca06594ce3b9eb9b53e5d35542954fb934
hisomura <hisomura.noreply@gmail.com>
2020-05-13 21:22:21 +0900

    1つのidentifierの処理を一つのループで納めるように修正

--- tokenize.c
+++ tokenize.c
@@ -1,11 +1,5 @@
 #include "9cc.h"
 
-int is_alnum(char c) {
-    return ('a' <= c && c <= 'z') ||
-           ('A' <= c && c <= 'Z') ||
-           ('0' <= c && c <= '9') ||
-           (c == '_');
-}
 
 // 新しいトークンを作成してcurに繋げる
 Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
@@ -21,6 +15,13 @@ static bool startswith(char *p, char *q) {
     return strncmp(p, q, strlen(q)) == 0;
 }
 
+static bool is_alpha(char c) {
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+}
+
+static bool is_alnum(char c) {
+    return is_alpha(c) || ('0' <= c && c <= '9');
+}
 
 // 入力文字列pをトークナイズしてそれを返す
 Token *tokenize(char *p) {
@@ -54,18 +55,18 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if ('a' <= *p && *p <= 'z') {
-            if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
-                cur = new_token(TK_RETURN, cur, p, 6);
-                p += 6;
-                continue;
-            }
-            if (cur->kind == TK_IDENT) {
-                cur->len += 1;
+        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+            cur = new_token(TK_RETURN, cur, p, 6);
+            p += 6;
+            continue;
+        }
+
+        // Identifier
+        if (is_alpha(*p)) {
+            char *q = p++;
+            while (is_alnum(*p))
                 p++;
-                continue;
-            }
-            cur = new_token(TK_IDENT, cur, p++, 1);
+            cur = new_token(TK_IDENT, cur, q, p - q);
             continue;
         }
         error_at(token->str, "トークナイズできません");
