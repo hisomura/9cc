be3554567ce292a221e9f973170a8ba00efab755
hisomura <hisomura.noreply@gmail.com>
2020-06-06 11:39:57 +0900

    consume()で該当したtokenを返却した方が色々使えて便利そうなので変更

--- parse.c
+++ parse.c
@@ -125,15 +125,14 @@ static Var *new_string_literal(char *p, int len) {
     return var;
 }
 
-// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
-// 真を返す。それ以外の場合には偽を返す。
-bool consume(char *op) {
+Token *consume(char *op) {
     if (token->kind != TK_RESERVED ||
         strlen(op) != token->len ||
         memcmp(token->str, op, token->len))
         return false;
+    Token *current = token;
     token = token->next;
-    return true;
+    return current;
 }
 
 Token *consume_ident() {
